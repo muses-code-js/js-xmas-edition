ecaf6094b184c076b9239868254c9f0f28763072
ReenaRajani <reena.rajani@gmail.com>
2017-02-09 14:04:35 +1100

    Clean up for level2

--- js/level2.js
+++ js/level2.js
@@ -1,4 +1,5 @@
 /*  Level 2 - Local storage
+After the user fills in the form and javascript validating
 
 1. Once the user enters the values, in case of success validation --
 	1. Add the values (name and the gift as an object along with a unique id) to local storage
@@ -15,20 +16,36 @@
 */
 
 /*
-We are getting information from the user but when we refresh the page, that information is lost
-and the user has to type in all the information again. Wouldn't it be nice if we could save the
-information that the user types somewhere and we could use that information to display something..
+
+We are getting information from the user and validating that information but when we refresh the page, that information is lost and the user has to type in all the information again. Wouldn't it be nice if we could save the information that the user types somewhere and we could use that information to display something..
 
 Here, the Local storage come to the rescue ( We can replace the local storage with an actual database
 when we will work with more advanced data. What the database does is that it provides a location to store
 the data and when needed we can access the data.. Think of a cookie jar which is the database and the cookie
 as the data ;) )
 
+So, what is a local storage ?
+it is a function which allows the web pages to store information locally on to the client's web browser using a key and value mechanism.
+Once we add the data to local storage,it will be available for ever unless we delete it
 
-*/
+You can view the information stored in the local Storage by
+ - opening the console window
+ - selecting the Application tab
+ - Within the Storage Menu, click on the down arrow near LocalStorage option
+ - select the file:// to view the local storage details saved s
 
+(go on, give it a try, chances are that you would not have any data yet..)
 
-/* Local Storage Notes
+There are 2 storage options available
+	1. window.localStorage - stores data with no expiration data
+	2. window.sessionStorage - stores data for one session (data is lost when the browser tab is closed)
+
+we will make use of the .localStorage object
+
+the basic operations for local storage is to add contents to it, get information from it and to clear the local storage entirely
+
+They can be done by
+
 
 Local storage - adding contents to local storage
  localStorage.setItem(‘key’, value);
@@ -47,20 +64,7 @@ Clear all the local storage
 window.localStorage.clear();
 
 
-
-Notes:
-
-Local storage is more secure, and large amounts of data can be stored locally, without affecting website performance. Unlike cookies, the storage limit is far larger (at least 5MB) and information is never transferred to the server.
-
-The localStorage object stores the data with no expiration date. The data will not be deleted when the browser is closed, and will be available the next day, week, or year.
-
-2 Objects for storing data
-	window.localStorage - stores data with no expiration data
-	window.sessionStorage - stores data for one session (data is lost when the browser tab is closed)
-
-
-
-Local storage can only save strings, so storing objects requires that they be turned into strings using JSON.stringify - you can’t ask local storage to store an object directly because it’ll store “[object Object]”, which isn’t right at all!
+Local storage can only save strings, so storing objects(read the Notes for for information about objects) requires that they be turned into strings using JSON.stringify - you can’t ask local storage to store an object directly because it’ll store “[object Object]”, which isn’t right at all!
 That also means the object must be run through JSON.parse on the way out of local storage
 e.g: localStorage.setItem('user', JSON.stringify({
     username: 'htmldog',
@@ -69,18 +73,32 @@ e.g: localStorage.setItem('user', JSON.stringify({
 
 var user = JSON.parse(localStorage.getItem('user'));
 
+Now lets use these information in to our code
+
+
+
+*/
+
+/*
+
+Notes:
+Advantages of using local storage
+ - more secure
+ - large amount of data can be stored locally and it would not affect the website performance and has the storage limit of atleast 5MB
+ - information is never transferred to the server
+ - data can be stored for ever, it does not have an expiry date
+ - data is not lost when the browser is closed and is available at any time unless we delete it manually
+
 Local storage is per origin (per domain and protocol). All pages, from one origin, can store and access the same data.
+
+
 
 * explain what is an object with properties.. how to get the key -value pairs ..
 * manipulating with objects - read,  write, find,
 make use of unique id to identify the local storage values ( .. )
-link to relational db for further readings..
+link to relational db for further reading
 
 
-You can view the information stored in the local Storage by
- - opening the console window
- - selecting the Application tab
- - Within the Storage Menu, click on the down arrow near LocalStorage option
- - select the file:// to view the local storage details saved s
+
 
 */
